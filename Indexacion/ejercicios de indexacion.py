c0b76df6e8f31d5e5114554131672486cb448911
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:24:40 -0500

    se agrego ejercicios de indexacion

---
+++ Indexacion/ejercicios de indexacion.py
@@ -0,0 +1,248 @@
+#INDEXACION 01
+
+#             10        20        30
+#    01234567890123456789012345678901234
+str="TUCUME ES EL VALLE DE LAS PIRAMIDES"
+
+#Acceso via indice: variable[idx]
+print(str[13]) # --> V
+print(str[14]) # --> A
+print(str[2])  # --> C
+print(str[29]) # --> A
+
+
+
+
+#INDEXACION 02
+
+#             10        20        30
+#    0123456789012345678901234567890123456
+str="EXCELENCIA COLLEGE COLEGIO PARTICULAR"
+
+#Acceso via indice: variable[idx]
+print(str[29]) # --> R
+print(str[35]) # --> A
+print(str[-1]) # --> R
+print(str[25]) # --> O
+
+
+
+
+#INDEXACION 03
+
+#             10        20        30        40
+#    012345678901234567890123456789012345678901234567
+str="Y ASI FUE COMO EL GUERRERO DESAPARECIO DE LA LUZ"
+
+#Acceso via indice: variable[idx]
+print(str[18]) # --> G
+print(str[19]) # --> U
+print(str[20]) # --> E
+print(str[21]) # --> R
+print(str[22]) # --> R
+print(str[23]) # --> E
+print(str[24]) # --> R
+print(str[25]) # --> O
+
+
+
+
+#INDEXACION 04
+
+#             10        20        30
+#    012345678901234567890123456789012
+str="TENGO DEMASIADO SUEÑO PARA SEGUIR"
+
+#Acceso via indice: variable[idx]
+print(str[6]) # --> D
+print(str[7]) # --> E
+print(str[8]) # --> M
+print(str[9]) # --> A
+print(str[10])# --> S
+print(str[11])# --> I
+print(str[12])# --> A
+print(str[13])# --> D
+print(str[14])# --> O
+
+
+
+
+#INDEXACION 05
+
+#             10        20        30
+#    012345678901234567890123456789012
+str="NUNCA TE DEBES RENDIR, TU PUEDESS"
+
+#Acceso via indice: variable[idx]
+print(str[0]) # --> N
+print(str[1]) # --> U
+print(str[2]) # --> N
+print(str[3]) # --> C
+print(str[4]) # --> A
+
+
+
+
+#INDEXACION 06
+
+#             10        20        30
+#    012345678901234567890123456789012345
+str="ERES LA MEJOR MADRE DE TODO EL MUNDO"
+
+#Acceso via indice: variable[idx]
+print(str[8])  # --> M
+print(str[9])  # --> E
+print(str[10]) # --> J
+print(str[11]) # --> O
+print(str[12]) # --> R
+
+
+
+
+#INDEXACION 07
+
+#             10        20        30
+#    012345678901234567890123456789012345678
+str="UNIVERSIDAD PEDRO RUIZ GALLO LAMBAYEQUE"
+
+#Acceso via indice: variable[idx]
+print(str[12]) # --> P
+print(str[13]) # --> E
+print(str[14])  # --> D
+print(str[15]) # --> R
+print(str[16]) # --> O
+
+
+
+#INDEXACION 08
+
+#             10        20        30
+#    0123456789012345678901234567890123
+str="NUESTRA SEÑORA DE LA MERCED TUCUME"
+
+#Acceso via indice: variable[idx]
+print(str[21]) # --> M
+print(str[22]) # --> E
+print(str[23]) # --> R
+print(str[24]) # --> C
+print(str[25]) # --> E
+print(str[26]) # --> D
+
+
+
+
+#INDEXACION 09
+
+#             10        20        30        40
+#    01234567890123456789012345678901234567890123
+str="SIEMPRE TE VOY A QUERER NO IMPORTA EL PASADO"
+
+#Acceso via indice: variable[idx]
+print(str[0]) # --> S
+print(str[1]) # --> I
+print(str[2]) # --> E
+print(str[3]) # --> M
+print(str[4]) # --> P
+print(str[5]) # --> R
+print(str[6]) # --> E
+
+
+
+
+
+#INDEXACION 10
+
+#             10        20        30
+#    01234567890123456789012345678901234
+str="DEBO PELEAR HASTA EL ULTIMO SEGUNDO"
+
+#Acceso via indice: variable[idx]
+print(str[5]) # --> P
+print(str[6]) # --> E
+print(str[7]) # --> L
+print(str[8]) # --> E
+print(str[9]) # --> A
+print(str[10])# --> R
+
+
+
+
+
+#INDEXACION 11
+
+#             10        20        30
+#    01234567890123456789012345678901
+str="HOLA GENTE DE YOUTUBE COMO ESTAN"
+
+#Acceso via indice: variable[idx]
+print(str[14]) # --> Y
+print(str[15]) # --> O
+print(str[16]) # --> U
+print(str[17]) # --> T
+print(str[18]) # --> U
+print(str[19]) # --> B
+print(str[20]) # --> E
+
+
+
+#INDEXACION 12
+
+#             10        20        30
+#    012345678901234567890123456789012
+str="COMO NO ENAMORARME DE ESA SONRISA"
+
+#Acceso via indice: variable[idx]
+print(str[26])  # --> S
+print(str[27])  # --> O
+print(str[28]) # --> N
+print(str[29]) # --> R
+print(str[30]) # --> I
+print(str[31]) # --> S
+print(str[32]) # --> A
+
+# Indexacion 13
+
+#             10        20        30        40        50
+#    0123456789012345678901234567890123456789012345678901
+str="ILLIMO TIERRA DEL TUMI DE ORO Y DE LA MIEL DE ABEJA"
+
+# Acceso via indice:Variable[idx]
+print(str[18]) # --> T
+print(str[19]) # --> U
+print(str[20]) # --> M
+print(str[21]) # --> I
+
+
+# Indexacion 14
+#              10        20
+#    0123456789012345678901
+str="INGENIERIA ELECTRONICA"
+
+# Acceso via indice: Variable[idx]
+print(str[11]) #---> E
+print(str[12]) #---> L
+print(str[13]) #---> E
+print(str[14]) #---> C
+print(str[15]) #---> T
+print(str[16]) #---> R
+print(str[17]) #---> O
+print(str[12]) #---> L
+print(str[19]) #---> I
+print(str[15]) #---> T
+print(str[16]) #---> R
+print(str[17]) #---> O
+
+
+# Indexacion 15
+#              10        20        30
+#    0123456789012345678901234567890123
+str="FELIZ NAVIDAD Y PROSPERO AÑO NUEVO"
+
+# Acceso via indice: Variable[idx]
+print(str[6]) #---> N
+print(str[7]) #---> A
+print(str[8]) #---> V
+print(str[9]) #---> I
+print(str[10]) #--->D
+print(str[11]) #--->A
+print(str[12]) #--->D
