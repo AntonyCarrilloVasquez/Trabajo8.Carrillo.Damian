dd1b89989bbc05b05b85f5f910c164cec24548b2
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:27:32 -0500

    se agrego ejercicios de comparacion

---
+++ comparacion/ejercicios de comparacion.py
@@ -0,0 +1,136 @@
+#comparacion 1
+#    0123456789
+str="Juan Pablo"
+#    0123456789012
+str="Pedro Ramirez"
+
+print("Juan Pablo" > "Pedro Ramirez")
+
+
+#comparacion 2
+#    01234567890123456789
+str="universidad de sipan"
+#    01234567890123456789012
+str="universidad de trujillo"
+
+print("universidad de sipan" < "universidad de trujillo")
+
+
+
+#comparacion 3
+#    0123456789012
+str="el oso polar"
+#    012345678901234567
+str="el oso de anteojos"
+
+print("el osopolar" != "el oso de anteojos")
+
+
+#comparacion 4
+#    01234567
+str="guitarra"
+#    0123456
+str="bateria"
+
+print("guitarra" > "bateria")
+
+
+#comparacion 5
+#    01234567890
+str="computadora"
+#    0123456
+str="celular"
+
+print("computadora" < "celular")
+
+
+#comparacion 6
+#    012345678901
+str="mano un free"
+#    0123456789012345
+str="es que la quiero"
+
+print("mano un free" == "es que la quiero")
+
+
+#comparacion 7
+#    012345
+str="escoba"
+#    012345678
+str="recogedor"
+
+print("escoba" > "recogedor")
+
+
+#comparacion 8
+#    012345678901234
+str="vale vale alok"
+#    01234567
+str="impureza"
+
+print("vale vale alok" < "impureza")
+
+
+#comparacion 9
+#    012345678
+str="promocion"
+#    012345678901234567
+str="excelencia college"
+
+print("promocion" > "excelencia college")
+
+
+#comparacion 10
+#    0123456789
+str="agua cielo"
+#    01234567890
+str="gaseosa oro"
+
+print("agua cielo" == "gaseosa oro")
+
+
+#comparacion 11
+#    012345
+str="basura"
+#    0123456789
+str="mi ex jaja"
+
+print("basura" != "mi ex jaja")
+
+
+#comparacion 12
+#    0123456789
+str="compeonato"
+#    01234567890123
+str="de lucha libre"
+
+print("campeonato" > "de lucha libre")
+
+
+#comparacion 13
+#    0123456
+str="samsung"
+#    01234567
+str="motorola"
+
+print("samsung" < "motorola")
+
+
+#comparacion 14
+#    0123456789012
+str="pedro ramirez"
+#    01234567890
+str="Juan castro"
+
+print("pedro ramirez" != "Juan castro")
+
+
+#comparacion 15
+#    0123456
+str="escanor"
+#    01234567
+str="estarosa"
+
+print("escanor" == "estarosa")
+
+
