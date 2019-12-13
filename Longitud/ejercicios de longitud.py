65da59f6930cd9b8b26b987f831e43841ef7ae3f
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:25:54 -0500

    se agrego ejercicios de longitud

---
+++ Longitud/ejercicios de longitud.py
@@ -0,0 +1,76 @@
+#Longitud 1
+nombre="Elmer"
+print(nombre)
+print(len(nombre))
+
+#Longitud 2
+frase="Solo sé que nada sé"
+print(frase)
+print(len(frase))
+
+#Longitud 3
+frase=" La union hace la fuerza "
+print(frase)
+print(len(frase))
+
+#Longitud 4
+nombre="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+print(nombre)
+print(len(nombre))
+
+#Longitud 5
+nombre="Antony Brayan Carrillo Vasquez"
+print(nombre)
+print(len(nombre))
+
+#Longitud 6
+cancion="YA LO PASADO PASADOOO NO ME INTERESAAA"
+print(cancion)
+print(len(cancion))
+print(len(""))
+
+# Longitud 7
+refran="CAMARON QUE SE DUERME SE LO LLEVA LA CORRIENTE"
+print(refran)
+print(len(refran))
+
+#Longitud 8
+Anuncio="EN ESTA NAVIDAD LLEVA TODO A MITAD DE PRECIO"
+print(Anuncio)
+print(len(Anuncio))
+
+#Longitud 9
+apellido="Suarez"
+print(apellido)
+print(len(apellido))
+
+#Longitud 10
+palabra="universidad"
+print(palabra)
+print(len(palabra))
+
+#Longitud 11
+significado="APARATO ELECTRONICO"
+print(significado)
+print(len(significado))
+
+#Longitud 12
+universidad="UNIVERSIDAD NACIONAL PEDRO RUIZ GALLO"
+print(universidad)
+print(len(universidad))
+
+#Longitud 13
+estudiante="Jorge Mario"
+print(estudiante)
+print(len(estudiante))
+
+#Longitud 14
+tipo_musica="electronica"
+print(tipo_musica)
+print(len(tipo_musica))
+
+# Longitud 15
+advertencia="no pasar por este lugar"
+print(advertencia)
+print(len(advertencia))
+
