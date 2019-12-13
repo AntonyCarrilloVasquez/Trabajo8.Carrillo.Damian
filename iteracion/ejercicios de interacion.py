35083f29dab02823eb129f46edfcc19e033eff46
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:29:11 -0500

    se agrego ejercicios de interacion

---
+++ iteracion/ejercicios de interacion.py
@@ -0,0 +1,92 @@
+#interacion 1
+nombre="ANTONY CARRILLO"
+for letra in nombre:
+    print(letra)
+
+
+#interacion 2
+universidad="LA CATOLICA"
+for letra in universidad:
+    print(letra)
+
+
+#interacion 3
+profesor="JUAN RODOLFO"
+for letra in profesor:
+    print(letra)
+
+
+#interacion 4
+raza="PASTOR ALEMAN"
+for letra in raza:
+    print(letra)
+
+
+#interacion 5
+musico="la mama coco :v"
+for letra in musico:
+    print(letra)
+
+
+#interacion 6
+marca="AGUA CIELO"
+for letra in marca:
+    print(letra)
+
+
+#interacion 7
+celular="SAMSUNG"
+for letra in celular:
+    print(letra)
+
+
+#interacion 8
+cancion="YA TE OLVIDE"
+for letra in cancion:
+    print(letra)
+
+
+#interacion 9
+libros="MATEMATICA BASICA"
+for letra in libros:
+    print(letra)
+
+
+#interacion 10
+colores="rojo"
+for letra in colores:
+    print(letra)
+
+
+#interacion 11
+apellidos="CARRILLO VASQUEZ"
+for letra in apellidos:
+    print(letra)
+
+
+#interacion 12
+tipos_cuaderno="stanford"
+for letra in tipos_cuaderno:
+    print(letra)
+
+
+#interacion 13
+carrera="ing.electronica"
+for letra in carrera:
+    print(letra)
+
+
+#interacion 14
+familia="CARRILLO"
+for letra in familia:
+    print(letra)
+
+
+#interacion 15
+rima="LA RANA SE SUBIO A UNA RAMA"
+for letra in rima:
+    print(letra)
+
+
+
+
