29dbe800ab39daf152c84b9d2719685b2cb43f45
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:29:42 -0500

    se agrego ejercicio de busqueda

---
+++ busqueda/ejercicios de busqueda.py
@@ -0,0 +1,49 @@
+
+#             10        20        30        40        50        60        70
+#    0123456789012345678901234567890123456789012345678901234567890123456789012345
+str="FELIZ NAVIDAD Y PROSPERO AÑO NUEVO LES DESEA LA UNIVERSIDAD PEDRO RUIZ GALLO"
+
+#busqueda 1
+print(str.find("PEDRO"))
+
+#busqueda 2
+print(str.index("UNIVERSIDAD"))
+
+#busqueda 3
+print(str.find("HOLA"))
+
+#busqueda 4
+print(str.index("DESEA"))
+
+#busqueda 5
+print(str.find("GALLO"))
+
+#busqueda 6
+print(str.find("ANTONY"))
+
+#busqueda 7
+print(str.index("U"))
+
+#busqueda 8
+print(str.find("G"))
+
+#busqueda 9
+print(str.index("DE"))
+
+#busqueda 10
+print(str.find("FE"))
+
+#busqueda 11
+print(str.find("AÑO"))
+
+#busqueda 12
+print(str.index("UNI"))
+
+#busqueda 13
+print(str.find("H"))
+
+#busqueda 14
+print(str.index("PROSPE"))
+
+#busqueda 15
+print(str.find("ELECTRONICA"))
