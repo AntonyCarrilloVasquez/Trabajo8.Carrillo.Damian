9fe805a398e3395226c99f177150ac13fe43e435
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:28:07 -0500

    se agrego ejercicios de concatenacion

---
+++ concatenacion/ejercicios de concatenacion.py
@@ -0,0 +1,107 @@
+#concatenacion 1
+str="arroz con leche"
+str="y mazamorra"
+print( "arroz con leche" + " " + "y mazamorra")
+
+
+#concatenacion 2
+print("helado" + " de " + "fresa")
+
+#concatenacion 3
+print("universidad" + " " + "nacional" + " " + "pedro ruiz gallo")
+
+
+#concatenacion 4
+#      0123456789012345678901234
+texto="espero que hoy esten bien"
+msg1=texto[11]+texto[12]+texto[13]
+msg2=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]+texto[5]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 5
+#      012345678901234567890123456
+texto="mis queridos alumnos lindos"
+msg1=texto[0]+texto[1]+texto[2]
+msg2=texto[21]+texto[22]+texto[23]+texto[24]+texto[25]+texto[26]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 6
+#      0123456789012345678901234567
+texto="queremos libertad para todos"
+msg1=texto[9]+texto[10]+texto[11]+texto[12]+texto[13]+texto[14]+texto[15]+texto[16]
+msg2=texto[23]+texto[24]+texto[25]+texto[26]+texto[27]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 7
+#      012345678901234567890
+texto="nunca te debes rendir"
+msg1=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]
+msg2=texto[15]+texto[16]+texto[17]+texto[18]+texto[19]+texto[20]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 8
+#      012345678901234567890123456
+texto="deben pagar todos por igual"
+msg1=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]
+msg2=texto[6]+texto[7]+texto[8]+texto[9]+texto[10]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 9
+#      01234567890123456789012345
+texto="mi mejor amigo es el mejor"
+msg1=texto[3]+texto[4]+texto[5]+texto[6]+texto[7]
+msg2=texto[9]+texto[10]+texto[11]+texto[12]+texto[13]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 10
+#      0123456789012345678901234
+texto="ojala alguien me quisiera"
+msg1=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]
+msg2=texto[17]+texto[18]+texto[19]+texto[20]+texto[21]+texto[22]+texto[23]+texto[24]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 11
+#      01234567890123456789012345
+texto="el agua es muuy importante"
+msg1=texto[3]+texto[4]+texto[5]+texto[6]
+msg2=texto[16]+texto[17]+texto[18]+texto[19]+texto[20]+texto[21]+texto[22]+texto[23]+texto[24]+texto[25]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 12
+#      012345678901234567890123456
+texto="tengo fe que todo esta bien"
+msg1=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]
+msg2=texto[6]+texto[7]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 13
+#      0123456789012345678901234
+texto="desearia tener mas fuerza"
+msg1=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]+texto[5]+texto[6]+texto[7]
+msg2=texto[19]+texto[20]+texto[21]+texto[22]+texto[23]+texto[24]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 14
+#      0123456789012345678901
+texto="tu te me quedas viendo"
+msg1=texto[9]+texto[10]+texto[11]+texto[12]+texto[13]+texto[14]
+msg2=texto[16]+texto[17]+texto[18]+texto[19]+texto[20]+texto[21]
+print(msg1 + " " +msg2)
+
+
+#concatenacion 15
+#      012345678901234567890123
+texto="ojala que hoy esten mal"
+msg1=texto[11]+texto[12]+texto[13]
+msg2=texto[0]+texto[1]+texto[2]+texto[3]+texto[4]+texto[5]
+print(msg1 + " " +msg2)
