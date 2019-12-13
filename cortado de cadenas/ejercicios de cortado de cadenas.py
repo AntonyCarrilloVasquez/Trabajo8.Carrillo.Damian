702bd491ea12dd18cb363c98abc0ef58e53c7cc7
AntonyCarrilloVasquez <56943321+antonycarrillovasquez@users.noreply.github.com>
2019-12-13 00:28:39 -0500

    se agrego ejercicios de corte de cadenas

---
+++ cortado de cadenas/ejercicios de cortado de cadenas.py
@@ -0,0 +1,50 @@
+
+#             10        20        30        40        50        60        70
+#    012345678901234567890123456789012345678901234567890123456789012345678901234567
+str="EN ESTE AÑO LES DESEO LO MEJOR DEL MUNDO Y QUE LA PASEN CON SUS SERES QUERIDOS"
+
+#cortado 1
+print( str[21:24] ) # --> LO
+
+#cortado 2
+print( str[25:30]) # --> MEJOR
+
+#cortado 3
+print(str[34::-3]) # --> DOMLEDEO SN
+
+#cortado 4
+print( str[1::-6]) # --> EO LO MEJOR DEL MUNDO Y QUE
+
+#cortado 5
+print( str[2:4] ) # -->  E
+
+#cortado 6
+print( str[51:53:-1]) # --> DOMLEDEO SN
+
+#cortado 7
+print(str[24::-1]) # --> OL OESED SEL OÑA ETSE NE
+
+#cortado 8
+print( str[1:26]) # --> N ESTE AÑO LES DESEO LO M
+
+#cortado 9
+print( str[61::-2] ) # --> U O EA LEQYONMLDRJMO EE E Ñ TEN
+
+#cortado 10
+print( str[::3]) # --> EEEÑL S  J LUO EAANOS R ED
+
+#cortado 11
+print(str[3::-1]) # --> E NE
+
+#cortado 12
+print( str[9::-6]) # --> ÑE
+
+#cortado 13
+print(str[21::-7]) # --> S E
+
+#cortado 14
+print( str[0:4]) # --> EN E
+
+#cortado 15
+print(str[:]) # --> EN ESTE AÑO LES DESEO LO MEJOR DEL MUNDO Y QUE LA PASEN CON SUS SERES QUERIDOS
+
